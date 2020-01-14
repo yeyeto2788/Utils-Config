343ffd161098fb58d1f7300d6a051dbf65e01598
yeyeto2788 <juanernestobiondi@gmail.com>
2020-01-14 16:06:54 +0100

    First initial working version of the module.

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(
+    name='Utils-Config',
+    version='1.0.0',
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description='Library to use .json files as configuration objects using namedtuple.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author='Juan Biondi',
+    author_email='juanernestobiondi@gmail.com',
+    python_requires='>=3.4.0',
+    url='https://github.com/yeyeto2788/Utils-Config',
+    packages=['utils_config'],
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
